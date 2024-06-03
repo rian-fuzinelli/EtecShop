5d055aa0a18c85b60e01d9862f2bba87dbbdd157
fuzinelli500280 <riancaiofuzinelli12@gmail.com>
2024-06-03 20:40:10 -0300

    Início

---
+++ EtecShop/ViewModels/HomeVM.cs
@@ -0,0 +1,9 @@
+using EtecShop.Models;
+namespace EtecShop.ViewModels;
+
+public class HomeVM
+{
+    public List<Categoria> Categorias { get; set; }
+    public List<Produto> Produtos { get; set; }
+
+}
